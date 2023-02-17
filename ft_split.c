193690004f3ddf652cc64e9bb427d8411aa3932e
Jeremy Hurpy <jeremyhurpy@gmail.com>
2023-02-17 20:05:13 +0700

    improvment after evaluation

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 17:39:35 by jeremy            #+#    #+#             */
-/*   Updated: 2023/02/14 00:25:46 by jhurpy           ###   ########.fr       */
+/*   Updated: 2023/02/17 20:03:14 by jhurpy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,14 +91,12 @@ char	**ft_split(char const *s, char c)
 	int		line;
 	int		i;
 
-	if (s == NULL)
-		return (NULL);
 	line = count_str(s, c);
 	tab = (char **)malloc(sizeof(char *) * (line + 1));
 	if (tab == NULL)
 		return (0);
 	if (s == NULL)
-		return ((char **) NULL);
+		return (NULL);
 	tab[line] = NULL;
 	i = 0;
 	while (i < line)
