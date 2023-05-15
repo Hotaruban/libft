c12d73c72db73a7ab9b2328749c30853c3078e17
Jeremy Hurpy <jeremyhurpy@gmail.com>
2023-05-15 12:22:28 +0700

    change atoi_base by hexatoi

--- src/ft_hexatoi.c
+++ src/ft_hexatoi.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*   ft_hexatoi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 00:53:40 by jhurpy            #+#    #+#             */
-/*   Updated: 2023/05/14 02:23:34 by jhurpy           ###   ########.fr       */
+/*   Updated: 2023/05/15 12:21:36 by jhurpy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,29 @@
 The function ft_atoi_base converts the string argument str (base N <= 16)
 */
 
-int		ft_atoi_base(const char *str)
+int	ft_hexatoi(const char *str)
 {
-	int		i;
-	int		nb;
-	char	*base;
+	int	result;
+	int	sign;
 
-	i = 0;
-	nb = 0;
-	base = "0123456789ABCDEFabcdef";
-	while (str[i])
+	result = 0;
+	sign = 1;
+	if (*str == '-')
 	{
-		nb = nb * 16 + ft_strchr(base, str[i]) - base; // convert the string to int
-		i++;
+		sign = -1;
+		str++;
 	}
-	return (nb);
+	while (ft_ishexa(*str))
+	{
+		result = result * 16;
+		if (*str >= '0' && *str <= '9')
+			result += (*str - '0');
+		else if (*str >= 'a' && *str <= 'f')
+			result += (*str - 'a' + 10);
+		else if (*str >= 'A' && *str <= 'F')
+			result += (*str - 'A' + 10);
+		str++;
+	}
+	return (result * sign);
 }
+
